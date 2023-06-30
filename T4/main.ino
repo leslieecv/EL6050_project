52cb846d91bf12485c394ac10e09fad384cbcc99
NRuminot <112728436+NRuminot@users.noreply.github.com>
2023-06-30 15:55:41 -0400

    Update main.ino

--- T4/main.ino
+++ T4/main.ino
@@ -20,6 +20,21 @@ int TRS_A = 37;                     ///< pin de hardware del transductor C
 int LRW_TX = 0;                     ///< pin de hardware del transmisor LoRaWAN
 int LRW_RX = 1;                     ///< pin de hardware del receptor LoRaWAN
 
+// Definición de los estados
+enum Estados { //Se define una enumeración para los estados
+  Energia,
+  Lidar,
+  Guardar1,
+  Trans,
+  Guardar2,
+  Lora,
+  Guardar3,
+  Error
+};
+
+// Variable para almacenar el estado actual
+Estados estadoActual;
+
 void pin_setup(){
     pinMode(LED_on, OUTPUT);
     pinMode(LED_red, OUTPUT);
@@ -29,13 +44,94 @@ void pin_setup(){
     pinMode(LED_trsc, OUTPUT);
 }
 
-void setup(){
 
-    pin_setup();
-
+void setup() {
+  // Inicialización del programa
+  estadoActual = Energia;
+  pin_setup();
 }
 
 void loop() {
+  // Lógica de los estados
+  switch (estadoActual) {
+    case Energia:
+      ///////////////////////
+      // E1 - ESTADO ENERGIA //
+      ///////////////////////
+      // Código para el estado Energia
+      // ...
+      // Cambio de estado
+      estadoActual = Lidar;
+      break;
 
+    case Lidar:
+      //////////////////////
+      // E2 - ESTADO LIDAR //
+      //////////////////////
+      // Código para el estado Lidar
+      // ...
+      // Cambio de estado
+      estadoActual = Guardar1;
+      break;
 
-} 
+    case Guardar1:
+      ////////////////////////////
+      // E3 - ESTADO GUARDAR 1 ///
+      ////////////////////////////
+      // Código para el estado Guardar1
+      // ...
+      // Cambio de estado
+      estadoActual = Trans;
+      break;
+
+    case Trans:
+      ///////////////////////
+      // E4 - ESTADO TRANS //
+      ///////////////////////
+      // Código para el estado Trans
+      // ...
+      // Cambio de estado
+      estadoActual = Guardar2;
+      break;
+
+    case Guardar2:
+      ///////////////////////////
+      // E5 - ESTADO GUARDAR 2 //
+      ///////////////////////////
+      // Código para el estado Guardar2
+      // ...
+      // Cambio de estado
+      estadoActual = Lora;
+      break;
+
+    case Lora:
+      //////////////////////
+      // E6 - ESTADO LORA //
+      //////////////////////
+      // Código para el estado Lora
+      // ...
+      // Cambio de estado
+      estadoActual = Guardar3;
+      break;
+
+    case Guardar3:
+      ///////////////////////////
+      // E7 - ESTADO GUARDAR 3 //
+      ///////////////////////////
+      // Código para el estado Guardar3
+      // ...
+      // Cambio de estado
+      estadoActual = Energia;
+      break;
+
+    case Error:
+      ///////////////////////
+      // E8 - ESTADO ERROR //
+      ///////////////////////
+      // Código para el estado Error
+      // ...
+      // Posible manejo del error
+      estadoActual = Energia;
+      break;
+  }
+}
