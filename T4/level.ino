818abd601badf1d2a1aa3a179c1184e2049eb23a
leslieecv <leslie.cardenas008@gmail.com>
2023-06-30 13:01:14 -0400

    update files

--- T4/level.ino
+++ T4/level.ino
@@ -1,26 +1,74 @@
-bool flagLevel1 = 0;
-bool flagLevel2 = 0;
-bool flagLevel3 = 0;
-bool flagLevel4 = 0;
 
 void level_status(){ // maquina para definir en que estado esta el nivel, configurar flags dependiendo de las mediciones
   
-  switch (level_status){
+    switch (stateLevel){
     
-    //////////////////
-    // E0 - LED OFF //
-    //////////////////
-    
-    case 0:
+        /////////////////////////////
+        // E0 - NO HAY INFORMACION //
+        ////////////////////////////
+        
+        case 0:
+            flagLevel0 = 1;
+            flagLevel1 = 0;
+            flagLevel2 = 0;
+            flagLevel3 = 0;
+            flagLevel4 = 0;
 
-      digitalWrite(LED_on, LOW);      // El pin del LED_on en OFF
+            if ( flagCheckStatus ){
+                level = getLevel();             // Realizar mediciones (aqui estoy media perdida si esperar o no la orden o darle no mas y como funcionaria esa flag)
+            }
+            if ( level > 1000 ){
+                flagLevel1 = 1;
+                flagLevel0 = 0;
+            }
+            else if ( level>800 && level<1000 ){
+                flagLevel2 = 1;
+                flagLevel0 = 0;
+            }
+            else if ( level>600 && level<800 ){
+                flagLevel3 = 1;
+                flagLevel0 = 0;
+            }
+            else if ( level<600 ){
+                flagLevel4 = 1;
+                flagLevel0 = 0;
+            }
+        break;
 
-      if ( flagStart ){               // Si se inicializan correctamente todos los sistemas
-        stateLED_on = 1;              // pasa al estado 1
-      }
-      else{                           // Si no, 
-        stateLED_on = 0;              // se mantiene en estado 0
-      }
-    break;
+        ////////////////////////
+        // E1 - ESTADO IDEAL //
+        ///////////////////////
+
+        case 1:
+            
+        break;
+
+        ////////////////////////
+        // E2 - ESTADO SEGURO //
+        ///////////////////////
+
+        case 2:
+
+        break;    
+        
+        
+        ///////////////////////////
+        // E3 - ESTADO NO SEGURO //
+        ///////////////////////////
+
+        case 3:
+
+        break;
+
+        ////////////////////////7
+        // E4 - ESTADO CRITICO //
+        /////////////////////////
+
+        case 4:
+
+        break;
+
+        default:
+            stateLevel = 0;
   }
 }
--- T4/lidar.ino
+++ T4/lidar.ino
@@ -8,6 +8,8 @@ uint16_t dist2 = 0;
 
 bool stateLidar = 0;
 
+int sensorLevel = 15;                 // Altura en metros a la que están los sensores respecto a la base del estanque
+
 void getLevel(){
     switch (stateLidar){
 
@@ -19,21 +21,25 @@ void getLevel(){
             if ( flagCheckStatus ){                 // Si se recibe la orden de medir
                 stateLidar = 1;                     // se pasa al estado 1
             }
+            else {                                  // Si no,
+                stateLidar = 0;                     // se mantiene en estado 0
+            }
         break;
 
         ///////////////////////////
         // E1 - MIDIENDO LIDAR 1 //
         ///////////////////////////
         case 1: 
-            lidar1.externalTrigger();               // Se solicita la medicion del LiDAR1
-            dist1 = lidar1.getDistance();           // y se recibe la distancia en cm 
+            lidar1.externalTrigger();               // Se solicita la medicion del LiDAR1,
+            dist1 = lidar1.getDistance();           // se recibe la distancia en cm 
+            dist1 = dist1* 0.01;                    // y se transforma a metros
 
             if ( dist1 = -1 ){                      // Si se detecta una medición erronea
                 stateLidar = 1;                     // se mantiene el estado
                 errorCount ++;                      // y se suma 1 al contador de errores
             }
-            else if ( errorCount >=3 ){              // Si se realizan más de 3 mediciones errones seguidas
-                stateLidar = 3;                     // se pasa al estado 3
+            else if ( errorCount > 5 ){             // Si se realizan más de 5 mediciones errones seguidas
+                stateLidar = 4;                     // se pasa al estado 4
             }
             else {
                 stateLidar = 2;                     // Se pasa al estado 2
@@ -45,25 +51,36 @@ void getLevel(){
         // E2 - MIDIENDO LIDAR 2 //
         ///////////////////////////
         case 2: 
-            lidar2.externalTrigger();               // Se solicita la medicion del LiDAR1
-            dist2 = lidar2.getDistance();           // y se recibe la distancia en cm 
+            lidar2.externalTrigger();               // Se solicita la medicion del LiDAR1,
+            dist2 = lidar2.getDistance();           // se recibe la distancia en cm
+            dist2 = dist2 * 0.01;                   // y se transforma a metros
 
-            if ( dist2 = -1 ){                      // Si se detecta una medición erronea
+            if ( dist2 = -1 ){                      // Si se detecta una medición erronea,
                 stateLidar = 2;                     // se mantiene el estado
                 errorCount ++;                      // y se suma 1 al contador de errores
             }
-            else if ( errorCount >=3 ){              // Si se realizan más de 3 mediciones errones seguidas
-                stateLidar = 3;                     // se pasa al estado 3
+            else if ( errorCount >5 ){              // Si se realizan más de 5 mediciones errones seguidas,
+                stateLidar = 4;                     // se pasa al estado 4
             }
             else {
-                stateLidar = 0;                     // Se vuelve al estado 0
+                stateLidar = 3;                     // Se vuelve al estado 0
                 errorCount = 0;                     // Contador de errores a 0
             }
         break;
-        /////////////////////////////
-        // E3 - 3 ERRORES SEGUIDOS //
-        /////////////////////////////
+
+        /////////////////////////
+        // E3 - CALCULO SIMPLE //
+        /////////////////////////
         case 3: 
+            dist = (dist1 + dist2) / 2;             // Se actualiza el nivel del estanque
+            level = dist - sensorLevel              // Se cambia la referencia de altura a la base
+            stateLidar = 0;
+        break;
+
+        /////////////////////////////
+        // E4 - 5 ERRORES SEGUIDOS //
+        /////////////////////////////
+        case 4: 
             
 
         default:
