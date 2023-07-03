81a30ced8723c802dd10d2b15456ab1b676c8ca4
NRuminot <112728436+NRuminot@users.noreply.github.com>
2023-07-03 15:34:41 -0400

    Update error.ino

--- T4/error.ino
+++ T4/error.ino
@@ -3,7 +3,7 @@
 void error(byte error_type, byte time_value) {      //
   EEPROM.begin();                                   // Inicializar la comunicación con la memoria EEPROM
   int pointer = 0;                                  // Se inicia pointer en cero, esta variable se usará para apuntar al espacio de memoria deseado
-  byte readValue = EEPROM.read(0);                  // Se inicia la variable readValue igual al contenido del primer espacio de memoria
+  byte readValue = EEPROM.read(pointer);                  // Se inicia la variable readValue igual al contenido del primer espacio de memoria
   while (readValue != 255) {                        // Siempre que readValue sea distinto de vacio
     pointer = pointer + 1;                          // Se aumenta en 1 el pointer
     readValue = EEPROM.read(pointer);               // Se actualiza el valor de readValue
