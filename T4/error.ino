2b18a20d26c03f69c0c1059138642948a7e833fc
NRuminot <112728436+NRuminot@users.noreply.github.com>
2023-07-03 13:37:48 -0400

    Update error.ino

--- T4/error.ino
+++ T4/error.ino
@@ -1,1 +1,14 @@
 #include <EEPROM.h>
+
+void error(){
+  EEPROM.begin();   // Inicializar la comunicación con la memoria EEPROM
+  int pointer = 0;
+  byte readValue = EEPROM.read(0);
+  while (readValue == 255) {
+    pointer = ponter + 1;
+    readValue = EEPROM.read(pointer);
+  EEPROM.write(pointer, error_type);
+  EEPROM.write(pointer+1, time_value);
+  }
+}
+  
