4066bd142d40a864ab36130ad16eeaa5ea21f3db
NRuminot <112728436+NRuminot@users.noreply.github.com>
2023-07-03 12:52:28 -0400

    Update main.ino
    agregue libreria eeprom para guardar errores en la memoria no volatil

--- T4/main.ino
+++ T4/main.ino
@@ -1,4 +1,5 @@
 #include <TFmini.h>
+#include <EEPROM.h>
 
 int LED_on = 42;                    ///< pin de hardware de LED de encendido
 int LED_red = 43;                   ///< pin de hardware de LED de red
